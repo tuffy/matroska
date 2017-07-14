01dd5e977466010972b45e7276f0394992392fa1
Brian Langenberger <bjl@usa.net>
2017-07-14 14:09:19 -0500

    Add preliminary Info chunk parser

---
+++ src/ids.rs
@@ -0,0 +1,8 @@
+pub const SEGMENT: u32 = 0x18538067;
+pub const INFO: u32 = 0x1549A966;
+pub const TITLE: u32 = 0x7BA9;
+pub const MUXINGAPP: u32 = 0x4D80;
+pub const WRITINGAPP: u32 = 0x5741;
+pub const DATEUTC: u32 = 0x4461;
+pub const TIMECODESCALE: u32 = 0x2AD7B1;
+pub const DURATION: u32 = 0x4489;
